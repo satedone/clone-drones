d73972934d53fb95d2cb8526bc7130cdcec22056
satedone <nz@intop.dp.ua>
2023-07-02 00:04:22 +0300

    correcter mistake with second script

--- main.js
+++ main.js
@@ -3,9 +3,3 @@ $(window).resize(function() {
 	let greenLineWidth = containerWidth * 0.23
 	$('.green_line').width(greenLineWidth);
 });
-
-$(window).resize(function() {
-	let containerWidth = $('.striker_title').width();
-	let greenLineWidth = containerWidth * 0.12
-	$('.gray_line').width(greenLineWidth);
-});
