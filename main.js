ffcfcaa6fc5d93f6487c56ae4bc5f6f2de0633b8
satedone <nz@intop.dp.ua>
2023-07-02 01:53:21 +0300

    finished header main about

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 $(window).resize(function() {
 	let containerWidth = $('.striker_title').width();
-	let greenLineWidth = containerWidth * 0.23
+	let greenLineWidth = containerWidth * 0.23;
+	let grayLineWidth = greenLineWidth / 2;
 	$('.green_line').width(greenLineWidth);
-});
+	$('.gray_line').width(grayLineWidth);
+  });
+  
