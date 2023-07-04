c7eef62f82aefc2b5cb9b8d17c6a7cd6a103bb99
satedone <nz@intop.dp.ua>
2023-07-04 11:16:14 +0300

    change green lines

--- main.js
+++
@@ -1,8 +0,0 @@
-$(window).resize(function() {
-	let containerWidth = $('.striker_title').width();
-	let greenLineWidth = containerWidth * 0.23;
-	let grayLineWidth = greenLineWidth / 2;
-	$('.green_line').width(greenLineWidth);
-	$('.gray_line').width(grayLineWidth);
-  });
-  
