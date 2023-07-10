7c3d2991f6ac53e515d2cc7228823ca5f3ea1bc8
satedone <nz@intop.dp.ua>
2023-07-10 23:51:11 +0300

    amazing awesome my first script carusel in testimonials

--- main.js
+++ main.js
@@ -50,3 +50,41 @@ nextButton.addEventListener('click', () => {
 window.addEventListener('resize', updateSlidesPerView);
 
 updateSlidesPerView();
+
+
+// testimonials script
+
+const testRounds = document.querySelectorAll('.test_round');
+const testTexts = document.querySelectorAll('.test_text');
+const testNames = document.querySelectorAll('.test_name');
+const testImages = document.querySelectorAll('.test_img');
+
+testRounds.forEach((round, index) => {
+  round.addEventListener('click', () => {
+    // При натисканні на test_round, змінюємо класи для відображення потрібного слайда
+    testTexts.forEach(text => {
+      text.classList.remove('active');
+    });
+    testTexts[index].classList.add('active');
+
+    testNames.forEach(name => {
+      name.classList.remove('active');
+    });
+    testNames[index].classList.add('active');
+
+    testImages.forEach(image => {
+      image.classList.remove('active');
+    });
+    testImages[index].classList.add('active');
+
+    testRounds.forEach(round => {
+      round.classList.remove('active');
+    });
+    testRounds[index].classList.add('active');
+
+  });
+});
+
+
+// testimonials script
+
